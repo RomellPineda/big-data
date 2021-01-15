ff11f57e31adb861d3abdcc568aea2cbc7439c82
Romell Pineda <romell.pineda@gmail.com>
2021-01-14 22:37:53 -0800

    implement scala using util

--- movieapp/src/main/scala/io/romellpineda/movieapp/ConnectUtil.scala
+++ movieapp/src/main/scala/io/romellpineda/movieapp/ConnectUtil.scala
@@ -2,21 +2,26 @@ package io.romellpineda.movieapp
 
 import java.sql.DriverManager
 import java.sql.Driver
+import java.sql.Connection
+import scala.util.Using
 
 object ConnectUtil {
 
   val connectionUrl = "jdbc:postgresql://localhost:5432/roml"
+  var connection : Connection = null
 
   def getConnection(): Unit = {
 
     classOf[org.postgresql.Driver].newInstance()
-    val conn = DriverManager.getConnection(connectionUrl)
-    val statement = conn.prepareStatement("SELECT * FROM player")
-    statement.execute()
-    val res = statement.getResultSet()
-    while (res.next()) {
-      println(res.getString("alias"))
+
+    Using.Manager { use =>
+      connection = use(DriverManager.getConnection(connectionUrl))
+      val dbQuery = use(connection.prepareStatement("SELECT * FROM player"))
+      dbQuery.execute()
+      val result = use(dbQuery.getResultSet())
+      while (result.next()) {
+        println(result.getString("alias"))
+      }
     }
-    conn.close()
   }
 }
