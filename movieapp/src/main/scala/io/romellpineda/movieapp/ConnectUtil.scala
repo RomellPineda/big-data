9038b0e28935d88c77eaac95ad1ae31e8b49258a
Romell Pineda <romell.pineda@gmail.com>
2021-01-19 01:53:33 -0800

    table creation

--- movieapp/src/main/scala/io/romellpineda/movieapp/ConnectUtil.scala
+++ movieapp/src/main/scala/io/romellpineda/movieapp/ConnectUtil.scala
@@ -4,26 +4,37 @@ import java.sql.DriverManager
 import java.sql.Driver
 import java.sql.Connection
 import scala.util.Using
+import scala.collection.mutable.ArrayBuffer
 
 object ConnectUtil {
 
   val connectionUrl = "jdbc:postgresql://localhost:5432/roml"
   var connection : Connection = null
-
-  def getConnection(): Unit = {
+  
+  def callDb(context : String): Unit = {
 
     classOf[org.postgresql.Driver].newInstance()
+    var resultArray = new ArrayBuffer[String]()
 
     Using.Manager { use =>
       connection = use(DriverManager.getConnection(connectionUrl))
-      val dbQuery = use(connection.prepareStatement("SELECT * FROM player"))
+      // send to processing function
+      val dbQuery = use(connection.prepareStatement(context))
       dbQuery.execute()
-
-      // target for clean up
-      val result = use(dbQuery.getResultSet())
-      while (result.next()) {
-        println(result.getString("alias"))
-      }
+      
     }
+
+    // Using.Manager { use =>
+    //   connection = use(DriverManager.getConnection(connectionUrl))
+    //   // send to processing function
+    //   val dbQuery = use(connection.prepareStatement("SELECT * FROM game"))
+    //   dbQuery.execute()
+
+    //   // target for clean up
+    //   val result = use(dbQuery.getResultSet())
+    //   while (result.next()) {
+    //     println(result.getString("title"))
+    //   }
+    // }
   }
 }
--- movieapp/src/main/scala/io/romellpineda/movieapp/Runner.scala
+++ movieapp/src/main/scala/io/romellpineda/movieapp/Runner.scala
@@ -4,7 +4,10 @@ object Runner {
 
   def main(args: Array[String]) : Unit = {
 
-    ConnectUtil.getConnection()
+    val createCustomerTable = "CREATE TABLE IF NOT EXISTS customer (customer_id SERIAL NOT NULL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, balance int DEFAULT 0);"
+    val createMovieTable = "DROP TABLE IF EXISTS movie CASCADE; CREATE TABLE movie (movie_id SERIAL NOT NULL PRIMARY KEY, title VARCHAR(255) NOT NULL, rating int DEFAULT 0);"
+    val createInvoiceTable = "DROP TABLE IF EXISTS invoice CASCADE; CREATE TABLE invoice (customer_id int REFERENCES customer (customer_id) ON UPDATE CASCADE ON DELETE CASCADE, movie_id int REFERENCES movie (movie_id) ON UPDATE CASCADE ON DELETE CASCADE);"
+    ConnectUtil.callDb(createInvoiceTable)
 
     // to do : build banner class + def returns Unit but prints movie rentals banner
     println("starting command line interface")
